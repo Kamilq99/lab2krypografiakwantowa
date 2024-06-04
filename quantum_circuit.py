23ea52e52387fae9f602eba17346e3a28413b7ae
Kamilq99 <83961352+Kamilq99@users.noreply.github.com>
2024-06-04 10:44:12 +0200

    poprawa zadania

--- quantum_circuit.py
+++ quantum_circuit.py
@@ -1,40 +1,29 @@
- 
-from qiskit import QuantumCircuit, Aer, execute
+import qiskit
 
-# Inicjalizacja obwodu kwantowego
-qc = QuantumCircuit(7)
+# Inicjalizacja qubitów
+q = qiskit.QuantumCircuit(6)
 
-# Krok 1: Implementacja bramek Hadamarda dla qubitów q0 - q5
-for i in range(6):
-    qc.h(i)
+# Stosowanie bramek Hadamarda na qubity 90-95
+q.h(2)
+q.h(3)
+q.h(4)
+q.h(5)
 
-# Krok 2: Sens działania bramek Hadamarda dla qubitów q0 - q5
-# Bramki Hadamarda wprowadzają qubity w superpozycję stanów |0⟩ i |1⟩
+# Stosowanie bramek CNOT na qubity 90 i 91, 91 i 92, 92 i 93, 93 i 94, 94 i 95
+q.cx(2, 3)
+q.cx(3, 4)
+q.cx(4, 5)
 
-# Krok 3: Implementacja operatora wyroczni z bramek CNOT
-# W tym przypadku, użyjemy bramek CNOT na qubitach q6 i q0 - q5
-for i in range(6):
-    qc.cx(6, i)
+# Stosowanie bramki X na qubit qe
+q.x(0)
 
-# Krok 4: Sens logiczny operatora wyroczni z bramek CNOT
-# Operator wyroczni pozwala na wykonanie operacji kontrolowanej, gdzie stan jednego qubitu (kontrolującego) 
-# wpływa na stan innego qubitu (docelowego) zgodnie z regułami bramek CNOT.
+# Pomiar qubitów 90-96
+q.measure_all()
 
-# Krok 5: Rola qubitu q6 i bramki X
-# Qubit q6 pełni rolę docelową dla operacji kontrolowanej przez pozostałe qubity.
-# Pierwsza operacja bramki X na qubicie q6 jest wymagana, aby przygotować go do operacji kontrolowanej.
+# Uruchamianie symulatora
+backend = qiskit.Aer.get_backend('qvm')
+job = qiskit.execute(q, backend, shots=1024)
+result = job.result().get_counts()
 
-# Krok 6: Zaprojektowanie i symulacja własnego operatora wyroczni
-# Tutaj możesz zaimplementować własne operatory wyroczni na podstawie wymagań i zinterpretować ich działanie.
-
-# Wyświetlenie obwodu
-print(qc)
-
-# Symulacja obwodu na komputerze klasycznym
-simulator = Aer.get_backend('qasm_simulator')
-job = execute(qc, simulator, shots=1000)
-result = job.result()
-
-# Wyświetlenie wyników symulacji
-counts = result.get_counts(qc)
-print("\nCounts:", counts)
+# Wyświetlanie wyników
+print(result)
